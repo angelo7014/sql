99b1a1848d79bdeeed9b97982ff7e90eb8b0a3f5
Angelo Mateus <angellomatteus@gmail.com>
2025-03-19 09:00:04 -0300

    Add files via upload

---
+++ sql/SQLite.2.sql
@@ -0,0 +1,6 @@
+INSERT INTO AGV (nome, turma, idade)
+VALUES
+('Aline','3DS',17),
+('Rebeca','3DS',17),
+('guilherme','3DS',18),
+('Kaylane','3DS',17);
---
+++ sql/SQLite.sql
@@ -0,0 +1,5 @@
+CREATE TABLE AGV (
+  id integer PRIMARY KEY,
+  nome TEXT NOT NULL, 
+  turma TEXT NOT NULL,
+  idade integer NOT NULL);
